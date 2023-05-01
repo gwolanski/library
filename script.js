808fd78ef557c4157fb24948f5d370d4c246e06c
gwolanski <gwol@udel.edu>
2023-05-01 07:15:11 -0600

    Basic setup, styling, and javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+let myLibrary = [];
+
+function openForm() {
+    document.getElementById("book-form").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("book-form").style.display = "none";
+}
+
+function Book() {
+  //the constructor
+}
+
+function addBookToLibrary() {}
+
+//create a button to add a book. onclick, have a pop up form
+//appear to enter in book info 
+
+//create a form with input field for book title, author, # of pages, whether it's been read,
+// and (optional) provide a rating, (optional) book image. have both submit and cancel buttons
+
+//on submit, have input fields get added to a table or card. might have to override usual
+//submit button function if doing actual submit button since it expects going to backend
+
+//once entry created, have ability to toggle read property on or off
+
+//have delete button to remove entries
